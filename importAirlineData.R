2f29144a35ea4b17c3072c1aa9f2fd5c4ce35f10
flaneuse <laura.d.hughes@gmail.com>
2016-06-20 19:58:04 -0400

    minor clean

--- importAirlineData.R
+++ importAirlineData.R
@@ -1,24 +1,29 @@
 # Exploring passenger airline dataset ----------------------------------------
+# Question: is Ronald Reagan National Airport stealing domestic passengers from
+# other area airports?
 
 # Laura Hughes, laura.d.hughes@gmail.com
 # 19 June 2016
 # (c) 2016 via MIT License
 
 
-# import data -------------------------------------------------------------
+# setup libraries, global variables ---------------------------------------
 library(dplyr)
 library(readxl)
 library(tidyr)
 library(ggplot2)
 
-# -- Individual Flight On-Time Arrivals --
-# Flight on-time data from the U.S. Bureau of Transportation Statistics
-# http://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236&DB_Short_Name=On-Time
-# Downloaded 19 June 2016
-flights = read.csv('data/On_Time_On_Time_Performance_2016_1.csv')
+airportCodes = c('DCA', 'IAD', 'BWI')
 
+# import data -------------------------------------------------------------
 # -- Airport location data, provided by Spotify --
 airports = read_excel('data/airports new.xls')
 
 # -- Carrier codes, provided by Spotify --
 carriers = read_excel('data/carriers.xls')
+
+# -- Individual Flight On-Time Arrivals --
+# Flight on-time data from the U.S. Bureau of Transportation Statistics
+# http://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236&DB_Short_Name=On-Time
+# Downloaded 20 June 2016
+flights = read.csv('data/On_Time_On_Time_Performance_2016_1.csv')
