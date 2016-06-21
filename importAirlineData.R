82209a460d3104cf302e3e16198386bbbad2f69e
flaneuse <laura.d.hughes@gmail.com>
2016-06-21 08:48:55 -0400

    updates

--- importAirlineData.R
+++ importAirlineData.R
@@ -14,9 +14,10 @@ library(tidyr)
 library(ggplot2)
 library(readr)
 library(zoo)
+library(data.table)
 
 airportCodes = c('DCA', 'IAD', 'BWI')
-
+airportCodes2 = c('ORD', 'LAX', 'JFK', 'LGA')
 
 # import data -------------------------------------------------------------
 # -- Airport location data, provided by Spotify --
@@ -39,7 +40,7 @@ readFlights = function(fileName, airports) {
   # -- Filter just the data from the interesting airports
   # (and get rid of irrelevant columns)
   flightData = flightData %>% 
-    filter(Origin %in% airports | Dest %in% airports) %>% 
+    filter(Origin %in% airports | Dest %in% airports) %>%
     select(Year, Quarter, Month, 
            DayofMonth, DayOfWeek, FlightDate,
            UniqueCarrier, Origin, OriginCityName, OriginState,
@@ -53,19 +54,49 @@ readFlights = function(fileName, airports) {
            NASDelay, SecurityDelay, LateAircraftDelay)
 }
 
+readAllFlights = function(fileName) {
+  flightData = read_csv(fileName)
+  
+  # (and get rid of irrelevant columns)
+  flightData = flightData %>% 
+    select(Year, Quarter, Month, 
+           DayofMonth, DayOfWeek, FlightDate,
+           UniqueCarrier, Origin, OriginCityName, OriginState,
+           Dest, DestCityName, DestState,
+           CRSDepTime, DepTime, DepDelay, DepDel15,
+           TaxiOut, WheelsOff, WheelsOn, TaxiIn,
+           CRSArrTime, ArrTime, ArrDelay, ArrDel15,
+           CRSElapsedTime, ActualElapsedTime,
+           Cancelled, CancellationCode, Diverted, AirTime,
+           Flights, Distance, CarrierDelay, WeatherDelay,
+           NASDelay, SecurityDelay, LateAircraftDelay)
+}
+
+
 # Read / filter the data from all the files
 flightList = lapply(flightFiles, function(x) readFlights(x, airports = airportCodes))
+
+flightList_chicago = lapply(flightFiles, function(x) readFlights(x, airports = airportCodes2))
+
+flightList_2015 = lapply(flightFiles[flightFiles %like% '2015'], function(x) readAllFlights(x))
+
+
+flightList_ref = lapply(flightFiles[flightFiles %like% '2015'], function(x) read_csv(x) %>% select(Year, Quarter, Month, 
+                                                                                                   DayofMonth, DayOfWeek, FlightDate,
+                                                                                                  Origin, Dest))
 
 # merge all the data together
 # flights = bind_rows(flightList) # dplyr thinks my data frames are corrupt?! :(
 flights = do.call(rbind, flightList) %>% 
   mutate(yr_month = zoo::as.yearmon(Year, Month))
 
+flights2015 = do.call(rbind, flightList_2015) %>% 
+  mutate(yr_month = zoo::as.yearmon(Year, Month))
 
 # Basic descriptive stats on data -----------------------------------------
 departures = flights %>% 
   filter(Origin %in% airportCodes) %>% 
-  group_by(Dest, yr_month) %>% 
+  group_by(Origin, yr_month) %>% 
   summarise(num = n(), dist = mean(Distance), 
             div = mean(Diverted), 
             cancelled = mean(Cancelled),
@@ -99,8 +130,27 @@ ggplot(subset, aes(x = AirTime, y = ActualElapsedTime)) +
 
 
 # Simple yearly trends ----------------------------------------------------
-ggplot(arrivals, aes(x = yr_month, y = num, group = Dest)) +
+ggplot(arrivals, aes(x = yr_month, y = num, 
+                     colour = Dest, group = Dest)) +
   geom_line() +
-  theme_bw() +
-  facet_wrap(~Dest)
+  theme_bw()
+
+ggplot(departures, aes(x = yr_month, y = num, 
+                     colour = Origin, group = Origin)) +
+  geom_line() +
+  theme_bw()
+
+
+# time on tarmac ----------------------------------------------------------
+View(flights2015 %>% 
+  group_by(Dest) %>% 
+  summarise(time = mean(TaxiIn, na.rm = TRUE),
+            num = n()) %>% 
+  arrange(desc(time)))
+
+View(flights2015 %>% 
+  group_by(Origin) %>% 
+  summarise(time = mean(TaxiOut, na.rm = TRUE),
+            num = n()) %>% 
+  arrange(desc(time)))
 
